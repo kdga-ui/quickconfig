e5cc130d70d22109daca4aee1167d33e2accf6ec
kdga <iamkdga@gmail.com>
2026-04-18 03:06:43 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,9 +1,4 @@
-getfenv().Custom_Blacklist = {
-    -- example "https://hi.com"
-} 
-
-getfenv().whitelist = {} -- example "https://google.com
 getfenv()._blockwebhook = false -- if you change to false, it will send the webhook but replace everything with "redacted" instead of ur info
 getfenv().DEBUG = false -- keep false, this is solely for my use with bugs
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/kdga-ui/anti-logger-v2/refs/heads/main/Anti%20logging.lua"))()
+loadstring(game:HttpGet("https://kdga-ui.github.io/scripts/anti-logger.lua"))()
