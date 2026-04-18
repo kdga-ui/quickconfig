b8d3efefd00910f7d5304eae4db7578bee817117
kdga <iamkdga@gmail.com>
2026-04-18 03:07:01 -0400

    Update config settings for webhook and debug

--- config.lua
+++ config.lua
@@ -1,4 +1,4 @@
-getfenv()._blockwebhook = false -- if you change to false, it will send the webhook but replace everything with "redacted" instead of ur info
-getfenv().DEBUG = false -- keep false, this is solely for my use with bugs
+getfenv()._blockwebhook = false
+getfenv().DEBUG = false
 
 loadstring(game:HttpGet("https://kdga-ui.github.io/scripts/anti-logger.lua"))()
